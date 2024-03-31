8adcd53137da27a13fa4ef90f19c08051246512f
Feekra Baset Baishakhi <65058830+FeekraBasetBaishakhi@users.noreply.github.com>
2024-03-31 15:01:02 +0600

    some script writting

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -7,7 +7,7 @@ public class BirdScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.name = "FeekraBrid";
     }
 
     // Update is called once per frame
