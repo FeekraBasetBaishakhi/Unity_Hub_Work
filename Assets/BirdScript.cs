c6787c068d74dc7b593813a31d27adc35da7c765
Feekra Baset Baishakhi <65058830+FeekraBasetBaishakhi@users.noreply.github.com>
2024-04-03 16:33:10 +0600

    working again movement pipe

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;// myRigidbody name for reference
-    //public float flapStrength;
+    public float flapStrength;
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.name = "FeekraBird";
+       
     }
 
     // Update is called once per frame
@@ -17,7 +17,7 @@ public class BirdScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) == true)
         {
-            myRigidbody.velocity = Vector2.up * 10;
+            myRigidbody.velocity = Vector2.up * flapStrength;
         }
     }
 }
