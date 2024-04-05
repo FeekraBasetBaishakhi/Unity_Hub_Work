6ca039c3323b80c9b9609feffb3a08bd1d4ad17e
Feekra Baset Baishakhi <65058830+FeekraBasetBaishakhi@users.noreply.github.com>
2024-04-06 00:02:14 +0600

    unity script for score increasing

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -7,11 +7,11 @@ public class LogicScript : MonoBehaviour
 {
     public int scorePlyer;
     public Text textScore;
-    
+    [ContextMenu("Increase Score")]//to run in unity
     public void  scoreAdd()// for showcasing number
     {
         scorePlyer = scorePlyer + 1;
-        textScore.text = scorePlyer.ToString();
+        textScore.text = scorePlyer.ToString();//
 
     }
     
