c5c35f0c3d1077f70226a257f5ae7dca04f11088
Feekra Baset Baishakhi <65058830+FeekraBasetBaishakhi@users.noreply.github.com>
2024-04-06 02:23:42 +0600

    some codes are done finishing yet to complete

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -6,18 +6,25 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;// myRigidbody name for reference
     public float flapStrength;
-    // Start is called before the first frame update
+    public LogicScript logic;
+    public bool birdIsAlive =true;
+    
     void Start()
     {
-       
+       logic =GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive ==true)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
