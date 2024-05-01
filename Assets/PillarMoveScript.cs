e7d68407d2ed124560e7a209d0347ed9f47a1f94
Feekra Baset Baishakhi <65058830+FeekraBasetBaishakhi@users.noreply.github.com>
2024-05-01 23:41:30 +0600

    Update Pillar

--- Assets/PillarMoveScript.cs
+++ Assets/PillarMoveScript.cs
@@ -14,7 +14,7 @@ public class PillarMoveScript : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;//ensure the multiplication happens the same, jekono frame rate er jonno and also needed prefrabricated gameobject
